df167b6f970548b7561c6b309e46a9798f10ca10
moohonk <charlieruiter@verizon.net>
2016-10-31 12:45:40 -0500

    Update audio-renderer.js

--- audio-renderer.js
+++ audio-renderer.js
@@ -148,7 +148,7 @@ function AudioRenderer()
         //size = (volume+0.125) * (volume+0.125) * BASE_DOT_SIZE + Math.random() * 2;
         //size *= 0.56;
         
-        size = Math.pow(volume + (1 - VOLUME_THRESH)/3, 2) * BASE_DOT_SIZE;
+        size = Math.pow(volume, 2) * BASE_DOT_SIZE;
         //size = 0.5;
         
         //Make some of the circles very big
