5d5bc713431e9cd9ad5ab00beefd21eaff4a5e8f
moohonk <charlieruiter@verizon.net>
2016-10-31 13:02:17 -0500

    Update audio-renderer.js

--- audio-renderer.js
+++ audio-renderer.js
@@ -152,9 +152,9 @@ function AudioRenderer()
         //size = 0.5;
         
         //Make some of the circles very big
-        if (Math.random() > 0.997) {
-          size *= (audioData[a] * 0.4) * Math.random();
-          volume *= Math.random() * 0.25;
+        if (Math.random() > 0.999) {
+          size *= (audioData[a] * 0.2) * Math.random();
+          volume *= Math.random() * 0.2;
         }
     
         var renderVals = {
