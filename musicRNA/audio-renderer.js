e6971564a068ba1dc144eaebfcab92c3795142f2
moohonk <charlieruiter@verizon.net>
2016-11-10 16:29:53 -0600

    Add files via upload

---
+++ musicRNA/audio-renderer.js
@@ -0,0 +1,212 @@
+/**
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ * For licensing see moohonk.github.io/musicRNA/LICENSE
+ */
+function AudioRenderer() 
+{
+  "use strict";
+  var TAU = Math.PI * 2;
+  var LOGBASE       = 32;   // The logbase used
+  var MAX_INDEX     = 850;  // The maximum index to look at in the frequency list
+  var LOWERBOUND    = 8;    // The value that BASE depends on
+  var REFLECT_NUM   = 0.25; // Precentage of the mapping to be on a reversed log scale
+  var BASE_DOT_SIZE = 1.0;  // The default dot radius
+  var BASE_ALPHA    = 0.09; // The base transparency for each dot
+  var VOLUME_THRESH = 0.675;// The lowest volume level for which we actually display something
+  var LOG_BASE  = Math.log(LOGBASE   );
+  // Shifts the mapping up on the log scale (compressing the frequencies) so it's easier to look at
+  var SHRINK    = Math.log(LOWERBOUND) / LOG_BASE; 
+  // The absolute maximum y-value a point can be mapped to
+  var maxLogVal = Math.log(128       ) / LOG_BASE; 
+  // The index before which the mapping is reflected
+  var MID_INDEX = (LOWERBOUND * Math.pow(128, REFLECT_NUM)); 
+  // The maximum height a frequency can be mapped to
+  var upperLog  = (Math.log(    MAX_INDEX) / LOG_BASE - SHRINK) / maxLogVal; 
+  // The minimum height a frequency can be mapped to
+  // Even though we're dealing with all of these crazy logs, they cancel out when solving for this.
+  var lowerLog  = 1/7 - REFLECT_NUM; 
+  // The height of the graph, used to scale it to nice values.
+  var normalizedHeight = (Math.log(2 * MAX_INDEX) / LOG_BASE - SHRINK) / maxLogVal - REFLECT_NUM;
+  console.log("normHeight:\t" + normalizedHeight);
+  var SHOULD_DRAW_STUFF = false;
+  var canvas = document.getElementById('render-area');
+  var ctx = canvas.getContext('2d');
+  var hasDrawnBackground = false;
+  var maxDist = 0;
+  var maxSize = 0;
+  var prevMax = 0;
+  var width  = 0;
+  var height = 0;
+  var imageWidth  = 0;
+  var imageHeight = 0;
+  // The percent of the screen we should leave for the border
+  var borderPercentX = 0.0625;
+  var borderPercentY = 0.25;
+  var yStart = 0;
+  var renderData = {
+    width: 0,
+    height: 0,
+    values: [],
+    maxWid: canvas.offsetWidth  * (1 - 2 * borderPercentX),
+    maxHgt: canvas.offsetHeight * (1 - 2 * borderPercentY),
+    bPX: borderPercentX,
+    bPY: borderPercentY
+  };
+
+  function onResize() 
+  {
+    width  = canvas.offsetWidth;
+    height = canvas.offsetHeight;
+
+    canvas.width  = width;
+    canvas.height = height;
+
+    renderData.width  = width;
+    renderData.height = height;
+    
+    imageWidth  = width  * (1 - 2 * borderPercentX);
+    imageHeight = height * (1 - 2 * borderPercentY);
+    yStart = imageHeight + height * borderPercentY ;
+
+    renderData.maxWid = canvas.offsetWidth  * (1 - 2 * borderPercentX);
+    renderData.maxHgt = canvas.offsetHeight * (1 - 2 * borderPercentY);
+
+    ctx.globalCompositeOperation = "lighter";
+  }
+
+  function clamp(val, min, max) {
+    return Math.min(max, Math.max(val, min));
+  }
+
+  this.clear = function() {
+    ctx.clearRect(0, 0, width, height);
+    hasDrawnBackground = false;
+    renderData.values.length = 0;
+    ctx.fillStyle = '#050505';
+    ctx.fillRect(borderPercentX * width, borderPercentY * height, imageWidth, imageHeight);
+    hasDrawnBackground = true;
+  };
+
+  this.render = function(audioData, normalizedPosition) 
+  {
+    if(!hasDrawnBackground)
+    {
+      ctx.fillStyle = '#050505';
+      ctx.fillRect(borderPercentX * width, borderPercentY * height, imageWidth, imageHeight);
+      hasDrawnBackground = true;
+    }
+    var lnDataDist = 0;
+    var normVol    = 0;
+    var volume     = 0;
+    var color      = 0;
+    var size       = 0;
+    
+    
+    var rectX = width * borderPercentX + imageWidth * normalizedPosition;
+    var rectY = 0.0;
+    var stuffHappened = false;
+    
+    if (normalizedPosition <= 1)
+    {
+      for (var a =  850; a >= 0; a--) 
+      {
+        // Normalize volume
+        volume = audioData[a] / 255;
+        
+        // Volume threshhold
+        if (volume < VOLUME_THRESH)
+          continue;
+        
+        // Map frequency to hue
+        color = Math.round(a * 360 / 1024);
+
+        // Map the point's y coordinate onto a log scale
+        lnDataDist = Math.log(a) / LOG_BASE - SHRINK;
+        rectY = yStart - imageHeight * lnDataDist / maxLogVal;
+        
+        // Check to see if we should use a flipped log scale to map this point
+        if (a < MID_INDEX)
+        {
+          // Move the beginning point to effect easier reflection
+          rectY = yStart - 2 * REFLECT_NUM * imageHeight;
+          
+          // Set the index used in position calculations 
+          //  to be the index reflected along y = MID_INDEX
+          var newInd = 2 * MID_INDEX - a;
+
+          lnDataDist = (Math.log(newInd) / LOG_BASE - SHRINK); 
+
+          // move away from the beginning point
+          rectY += imageHeight * lnDataDist / maxLogVal;
+        }
+        
+      // Scaling Stuff
+        // Translate all y Coords for easier scaling
+        rectY -= (yStart - imageHeight * upperLog);
+
+        // Do some scaling
+        rectY /= normalizedHeight;
+
+        // Untranslate the y coords into their final form
+        rectY += height * borderPercentY;
+        
+        if(rectY > maxDist)
+        {
+          maxDist = rectY; 
+        }
+        
+        
+        
+        // Size computation
+        //size = (volume+0.125) * (volume+0.125) * BASE_DOT_SIZE + Math.random() * 2;
+        //size *= 0.56;
+        
+        
+        size = Math.pow(volume + 0.125, 2) * BASE_DOT_SIZE;
+        
+        //size = 0.5;
+        
+        //Make some of the circles very big
+        /*
+        if (Math.random() > 0.999) {
+          size *= (audioData[a] * 0.2) * Math.random();
+          volume *= Math.random() * 0.2;
+        }
+        */
+    
+        var renderVals = {
+          //alpha: volume * volume * 0.09,
+          alpha: Math.pow(volume + (1-VOLUME_THRESH - 0.1), 2) * BASE_ALPHA,
+          color: color,
+          x: rectX,
+          y: rectY,
+          size: size
+        };
+        
+        // Draw the point on the canvas
+        ctx.globalAlpha = renderVals.alpha;
+        ctx.fillStyle = 'hsl(' + renderVals.color + ', 80%, 50%)';
+        ctx.beginPath();
+        ctx.arc(rectX, rectY, renderVals.size, 0, TAU, false);
+        ctx.closePath();
+        ctx.fill();
+
+        renderData.values.push(renderVals);
+      }
+      if(prevMax != maxDist)
+      {
+        prevMax = maxDist;
+        console.log("minDist:\t" + maxDist / height);
+      }
+    }
+  };
+
+  this.getRenderData = function() {
+    return renderData;
+  };
+
+  window.addEventListener('resize', onResize, false);
+  window.addEventListener('load', function() {
+    onResize();
+  }, false);
+}
