d8f08733a09b3c893068625df8aae6fc4859f9ad
moohonk <charlieruiter@verizon.net>
2016-11-14 11:40:55 -0600

    Update bootstrap.js

--- musicRNA/bootstrap.js
+++ musicRNA/bootstrap.js
@@ -54,6 +54,7 @@ window.requestAnimFrame =
     musicRNA.parse(file);
     fileDropArea.classList.add('dropped');
     console.log("Go");
+    musicRNA.hasDisplayedStats = false;
     ID3.loadTags("filename.mp3", function() {
 
       // Determine the name and the author of this song
