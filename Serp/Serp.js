aec845e26a400cbb510b4d0cbda3dad2a5f8e1cb
moohonk <charlieruiter@verizon.net>
2019-10-17 18:04:19 -0500

    Rule from user input

--- Serp/Serp.js
+++ Serp/Serp.js
@@ -7,10 +7,8 @@ function Serp(){
     var cx = 0;
     var cy = 0;
     var len = 512;
-    var maxDepth = 8;
+    var maxDepth = 4;
     var rule = [0, 0, 0];
-
-    
 
     // Lists storing all necessary information to draw any one iteration 
     var URXCoords = [[0]];
@@ -21,6 +19,13 @@ function Serp(){
     // We'll draw the fractal here
     var canvas = document.getElementById("canvas");
     var c = canvas.getContext('2d');
+
+    var rule0 = document.getElementById("0Input");
+    var rule1 = document.getElementById("1Input");
+    var rule2 = document.getElementById("2Input");
+    rule0.addEventListener("input", function(){processInput(0);});
+    rule1.addEventListener("input", function(){processInput(1);});
+    rule2.addEventListener("input", function(){processInput(2);});
 
     // Given the rotation of the parent square and the child square, 
     //  these lists give the offsets of the upper left corners of the child squares
@@ -65,81 +70,75 @@ function Serp(){
         lenList[i] = l;
     }
     
+    function updateRule(newRule)
+    {
+        rule[0] = newRule[0];
+        rule[1] = newRule[1];
+        rule[2] = newRule[2];
+        onResize();
+    }
 
     // Called when the user clicks on a subsquare
     //  Changes the current rule and redraws the fractal
     function selectSubsquare(quadrant, subsquare)
     {
-        var order = [0, 3, 1, 2];
-        console.log("quadrant", quadrant);
-        console.log("subsquare", subsquare);
-        if (rule[quadrant-1] == order[subsquare])
+        // If we clicked on an already-selected subsquare, 
+        //  we don't need to calculate anything
+        if (rule[quadrant-1] == subsquare)
             return;
-        // Find currently selected subsquare
+        
+        console.log("quadrant: " + quadrant + ", subsquare: " + subsquare);
+        // Deselect all subsquares in the quadrant
         for (var i = 0; i < 4; i++)
         {
-            var sub = document.getElementById("q" + quadrant + "." + i);
+            var sub = document.getElementById("q" + quadrant + "-" + i);
+            // console.log("q" + quadrant + "-" + i);
             sub.classList.remove("selected");
         }
-        var id = "q" + (quadrant) + "." + subsquare;
+
+        // Select the subsquare we just clicked
+        var id = "q" + (quadrant) + "-" + subsquare%4;
         console.log(id);
         document.getElementById(id).classList.add("selected");
 
-        // Deselect it
+        // Update the rule
+        rule[quadrant-1] = subsquare;
 
-        // Select this subsquare
+        // Update the value of the input element that was changed
+        document.getElementById((quadrant-1) + "Input").value = subsquare;
 
-        rule[quadrant-1] = order[subsquare];
-        console.log("new rule: ", rule);
-
-        onResize();
-
-        drawDepth(1);
-
-        // // Recalculate the fractal
-        // SierpIterUpTo(maxDepth);
-
-        // // Draw the fractal
-        // drawUpTo(maxDepth);
+        updateRule(rule);
     }
 
     function createRuleButton(i, j)
     {
         var order = [3, 1, 2];
+
+        // The parent
+        var quadrant = document.getElementById("q" + (i+1));
+
+        // Create a new rule div
         var subsquare = document.createElement("div");
         subsquare.classList.add("rule");
+        subsquare.id = "q" + order[i] + "-" + j;
+
+        // If this subsquare is the first in this quadrant, select it initially
         if(j == 0)
             subsquare.classList.add("selected");
-        subsquare.id = "q" + order[i] + "." + j;
+
+        // When the user clicks on a subsquare, it should become selected
         subsquare.addEventListener("click", function(){selectSubsquare(order[i], j)});
-        var quadrant = document.getElementById("q" + (i+1));
+        
         quadrant.appendChild(subsquare);
     }
+
     // Create the rule UI
     function createRuleButtons()
     {
-        
         for (var i = 0; i < 3; i++)
-        {
             for (var j = 0; j < 4; j++)
-            {
                 createRuleButton(i, j);
-            }
-        }
     }
-    
-    // Given the current rotation of the square and the rule, 
-    //    determine where the UR corners of child squares are
-    // function getChildOffsets(R, rule, length)
-    // {
-    //     //Return these values
-    //     dx0 = x0[R][rule[0]];
-    //     dy0 = y0[R][rule[0]];
-    //     dx1 = x1[R][rule[1]];
-    //     dy1 = y1[R][rule[1]];
-    //     dx2 = x2[R][rule[2]];
-    //     dy2 = y2[R][rule[2]];
-    // }
 
     // Called whenever the window changes size
     function onResize()
@@ -147,7 +146,7 @@ function Serp(){
         width  = canvas.offsetWidth;// - (canvas.offsetWidth % 512);
         height = canvas.offsetHeight;// - (canvas.offsetHeight % 512);
 
-
+        if (height % 2 == 1) height = height - 1;
 
         URXCoords[0] = [width  / 2 - len / 2];
         URYCoords[0] = [height / 2 - len / 2];
@@ -167,13 +166,9 @@ function Serp(){
         drawUpTo(maxDepth);
     }
 
-    // function drawBackground()
-    // {
-        
-    // }
-
     function drawSquare(x, y, length, color)
     {
+        // Truncate decimal values so we don't have to deal with antialiasing
         var x1 = x - x % 1;
         var y1 = y - y % 1;
         c.fillStyle = color;
@@ -185,15 +180,12 @@ function Serp(){
         drawSquare(x, y, length, color);
     };
 
-
     // Non-Recursive Function: Sierpinski iteration
     // Accesses + Modifies a global array containing the UR corners of each successive depth level
 
     // Given the current fractal depth, create the next level down
     function SierpIterOnce(depth)
     {
-        console.log("Sierp depth ", depth);
-
         // Access coordinates in UR corner list
         var xC = URXCoords[depth];
         var yC = URYCoords[depth];
@@ -201,29 +193,33 @@ function Serp(){
         var newL = lenList[depth] / 4;
 
         // Calculate offsets for the current depth
-        var x00 = x0;
-        var x11 = x1;
-        var x22 = x2;
-        var y00 = y0;
-        var y11 = y1;
-        var y22 = y2;
+        var x00 = [[],[],[],[]];
+        var x11 = [[],[],[],[]];
+        var x22 = [[],[],[],[]];
+        var y00 = [[],[],[],[]];
+        var y11 = [[],[],[],[]];
+        var y22 = [[],[],[],[]];
+
+        // Multiply everything outside of the loop
+        //  Slows down the first couple iterations but saves time later
         for(var i = 0; i < 4; i++)
         {
             for(var j = 0; j < 4; j++)
             {
-                x00[i][j] = x00[i][j] * newL;
-                x11[i][j] = x11[i][j] * newL;
-                x22[i][j] = x22[i][j] * newL;
-                y00[i][j] = y00[i][j] * newL;
-                y11[i][j] = y11[i][j] * newL;
-                y22[i][j] = y22[i][j] * newL;
-
+                x00[i][j] = x0[i][j] * newL;
+                x11[i][j] = x1[i][j] * newL;
+                x22[i][j] = x2[i][j] * newL;
+                y00[i][j] = y0[i][j] * newL;
+                y11[i][j] = y1[i][j] * newL;
+                y22[i][j] = y2[i][j] * newL;
             }
         }
 
-        URXCoords[depth+1] = [];
-        URYCoords[depth+1] = [];
-        rotList  [depth+1] = [];
+        var newDepth = depth + 1;
+
+        URXCoords[newDepth] = [];
+        URYCoords[newDepth] = [];
+        rotList  [newDepth] = [];
 
         // Loop over all squares in the current fractal depth
         for (var i = 0; i < rots.length; i++)
@@ -248,72 +244,71 @@ function Serp(){
             var r2 = (R + rule[2]) % 4;
 
             // Push the children onto the next fractal depth
-            URXCoords[depth+1] = URXCoords[depth+1].concat([xc0, xc1, xc2]);
-            URYCoords[depth+1] = URYCoords[depth+1].concat([yc0, yc1, yc2]);
-            rotList  [depth+1] = rotList  [depth+1].concat([ r0,  r1,  r2]);
+            // Don't use .concat() -- it's around 700x slower
+            var start = URXCoords[newDepth].length;
+            URXCoords[newDepth][start  ] = xc0;
+            URXCoords[newDepth][start+1] = xc1;
+            URXCoords[newDepth][start+2] = xc2;
+
+            URYCoords[newDepth][start  ] = yc0;
+            URYCoords[newDepth][start+1] = yc1;
+            URYCoords[newDepth][start+2] = yc2;
+
+            rotList  [newDepth][start  ] = r0;
+            rotList  [newDepth][start+1] = r1;
+            rotList  [newDepth][start+2] = r2;
         }
     }
 
-
     // Given a depth and a rule, do sierpinski gasket up to that depth
-    //  Store information in relevant lists
     function SierpIterUpTo(depth)
     {
-        // for each fractal depth
         for(var d = 0; d < depth; d++)
-        {
             SierpIterOnce(d);
-        }
     }
 
     function drawDepth(depth)
     {
         var xC = URXCoords[depth];
         var yC = URYCoords[depth];
-        colors = ['#000000','#0a0a0a','#141414','#1e1e1e','#282828','#323232', '#3c3c3c', '#464646', '#505050'];
+        //colors = ['#000000','#ff0000','#00ff00','#0000ff','#ffff00','#ff00ff', '#00ffff', '#ffff80', '#ff80ff', '80ffff'];
+        color = '#000';
         for(var i = 0; i < xC.length; i++)
-        {
-            drawSquare(xC[i], yC[i], lenList[depth] / 2, colors[depth]);
-        }
+            drawSquare(xC[i], yC[i], lenList[depth] / 2, color);//colors[depth]);
     }
 
     function drawUpTo(depth)
     {
+        // Sometimes, a line shows up at the vary bottom of the fractal
+        drawSquare(URXCoords[0][0], URYCoords[0][0]+lenList[0], lenList[0], '#000000');
         for (var d = 0; d < depth; d++)
-        {
             drawDepth(d);
-        }
     }
 
-    // A rule consists of three numbers corresponding to how much each quadrant should be rotated
-    // 0 -> no change
-    // 1 -> +90 degrees
-    // 2 -> +180 degrees
-    // 3 -> +270 degrees
+    // Called when the [i]th text input box has been changed
+    // Sanitizes the input and updates the rule
+    function processInput(i)
+    {
+        console.log("processing input");
+        var inputs = [rule0, rule1, rule2];
 
-    // Want to freely go forward or backward in 
+        var r = inputs[i];
 
-    // FUNCTION -- 
+        // If someone types a new number without selecting the old number, remove the old number
+        if(r.value >  10) r.value = (1 * r.value) % 10;
 
-    // each fractal chunk has a corner position and a rotation associated with it
-    // iterating once 
-    // length is divided by two
+        // If a number is one above or one below the rule domain, loop back around
+        if(r.value ==  4) r.value = 0;
+        if(r.value == -1) r.value = 3;
 
-    // FUNCTION A -- given depth level, find how many positions we should have in the lists already
+        // If a number is out of the rule domain, put it back into the rule domain
+        if(r.value >   3) r.value = 3;
+        if(r.value <   0) r.value = 0;
 
-    // loop for each position in this depth level, starting at A(depth-1) up to A(depth)
+        // Cast r.value to an integer from a string
+        selectSubsquare(i+1, 1 * r.value);
+    }
 
-    // FUNCTION B -- given rotation and position, find positions and rotations of child quadrants
-
-    // push child positions and rotations onto their respective lists
-
-
-
-
-    // Drawing
-    //  Start with a background color
-    //  fill in top right quadrant with black
-    //  keep doing that
     window.addEventListener('load', function() {
         onResize();
         createRuleButtons();
